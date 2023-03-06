5ef6fbe98c9ca9d802e0666e3b217a431abba1c3
BernardWasTaken <bernard.kuznik@gmail.com>
2023-03-06 21:52:45 +0100

    init

---
+++ src/main/java/com/example/main.java
@@ -0,0 +1,70 @@
+package com.example;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+public class main {
+    
+    @FXML
+    Button side_profile;
+    @FXML
+    Button side_dashboard;
+    @FXML
+    Button side_settings;
+    @FXML
+    Pane profile_pane;
+    @FXML
+    Pane dashboard_pane;
+    @FXML
+    Pane settings_pane;
+
+    @FXML
+    private void initialize() {
+        if (profile_pane == null) {
+            FXMLLoader loader = new FXMLLoader(App.class.getResource("main.fxml"));
+            loader.setController(this);
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("error");
+            }
+        }
+    }
+
+    @FXML
+    private void profile_Click() throws IOException{
+        side_profile.setId("tab-selected");
+        side_dashboard.setId("tab-unselected");
+        side_settings.setId("tab-unselected");
+
+        profile_pane.setVisible(true);
+        dashboard_pane.setVisible(false);
+        settings_pane.setVisible(false);
+    }
+
+    @FXML
+    private void dashboard_Click() throws IOException{
+        side_profile.setId("tab-unselected");
+        side_dashboard.setId("tab-selected");
+        side_settings.setId("tab-unselected");
+
+        profile_pane.setVisible(false);
+        dashboard_pane.setVisible(true);
+        settings_pane.setVisible(false);
+    }
+
+    @FXML
+    private void settings_Click() throws IOException{
+        side_profile.setId("tab-unselected");
+        side_dashboard.setId("tab-unselected");
+        side_settings.setId("tab-selected");
+
+        profile_pane.setVisible(false);
+        dashboard_pane.setVisible(false);
+        settings_pane.setVisible(true);
+    }
+}
